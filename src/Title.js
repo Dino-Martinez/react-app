a7693d8c96af76f506189c60b3adede1c7d7026b
Secundino Martinez <Dino.Martinez.514@gmail.com>
2020-11-18 16:53:55 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,17 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css'
+
+function Title(props) {
+  return (
+    <div className="Title">
+      <h1>SFPOPS</h1>
+      <div className="Title-Subtitle">
+          <h3>San Francisco Privately Owned Public Open Spaces</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Title
